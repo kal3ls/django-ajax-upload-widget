9eaa6696341569c97ea1e6511106920e7bdfbda4
Domin Adrian Morales Chacon <adrian@nextcenturypublishing.com>
2016-09-12 16:25:35 -0400

    Add customizable field AJAX_UPLOAD_FILE_FIELD_SIZE_MB

--- ajax_upload/forms.py
+++ ajax_upload/forms.py
@@ -3,7 +3,7 @@ import uuid
 from django import forms
 
 from ajax_upload.models import UploadedFile
-
+from ajax_upload.settings import FILE_FIELD_MAX_SIZE_MB
 
 class UploadedFileForm(forms.ModelForm):
 
@@ -16,6 +16,6 @@ class UploadedFileForm(forms.ModelForm):
         # Change the name of the file to something unguessable
         # Construct the new name as <unique-hex>-<original>.<ext>
         data.name = u'%s-%s' % (uuid.uuid4().hex, data.name)
-        if data.size * 9.53674e-7 > 2.0:
+        if data.size * 9.53674e-7 > FILE_FIELD_MAX_SIZE_MB and FILE_FIELD_MAX_SIZE_MB != 0:
 	        raise forms.ValidationError('The image is too big! (Maximum size allowed: 2MB)')
         return data
